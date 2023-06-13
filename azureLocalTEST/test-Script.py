4839ddab60bfe77988d26a705de0b4bc8082e7aa
JTxCo <103444631+JTxCo@users.noreply.github.com>
2023-06-13 18:08:17 -0500

    Azure emulator connection made

--- azureLocalTEST/test-Script.py
+++ azureLocalTEST/test-Script.py
@@ -1,21 +1,26 @@
 import pyodbc
 
+server_name = "localhost"
+database_name = "azureLocalTEST"
+user_id = "testLogin"
+password = "$testPassword"
+
 conn_str = (
-    "Driver={ODBC Driver 17 for SQL Server};"
-    "Server=localhost;"
-    "Database=azureLocalTEST;"
-    "Trusted_Connection=yes;"
+    f"Driver={{ODBC Driver 17 for SQL Server}};"
+    f"Server={server_name};"
+    f"Database={database_name};"
+    f"UID={user_id};"
+    f"PWD={password};"
 )
 
 
 conn = pyodbc.connect(conn_str)
 cursor = conn.cursor()
 cursor.execute("SELECT * FROM [dbo].[table1]")
-rows = cursor.fetchone()
+rows = cursor.fetchall()
 
 for row in rows:
-    print(row)
-    
+    print("row: " +str(row))
     
 cursor.execute("SELECT @@version")
 row = cursor.fetchone()

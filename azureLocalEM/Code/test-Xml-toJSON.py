3db6f7e26fb7d7b5f5c52dcb8acfde95eafe00f7
JTxCo <103444631+JTxCo@users.noreply.github.com>
2023-06-23 20:43:52 -0500

    xml parsing started

--- azureLocalEM/Code/test-Xml-toJSON.py
+++ azureLocalEM/Code/test-Xml-toJSON.py
@@ -1,12 +1,17 @@
 import xmltodict
 import json
 import os
-# with open('./Data/BioMems-XML-example.xml') as f:
-#     data = xmltodict.parse(f.read())
-#     json_data = json.dumps(data)
-#     print(json_data)
+with open('../Data/BioMems-XML-example.xml') as f:
+    data = xmltodict.parse(f.read())
+    json_data = json.dumps(data)
+    print(json_data)
 
-cwd = os.getcwd()
-path = ''
-files = os.listdir()
-print("Files in %r. the path: %r : files: %s" % (cwd,path, files))
+
+
+
+# path = '../Data/'
+# cwd = os.getcwd()
+# filesCWD = os.listdir(cwd)
+# filesPath = os.listdir(path)
+# print(f"Files in {path}: {filesPath}")
+# print(f"Files in swd: {filesCWD}")
--- azureLocalEM/Code/xml-Parse.py
+++ azureLocalEM/Code/xml-Parse.py
@@ -6,9 +6,25 @@ tree = ET.parse('../Data/BioMems-XML-example.xml')
 # Get the root element
 root = tree.getroot()
 # It erate over child elements
-
-for child in root:
-    print(f"tag: {child.tag}, attribute: {child.attrib}")
+sampleData = root.find('./sampleData')
+for well in sampleData.findall('*'):
+    print(well.tag)
+    print('  Well Data:', well.text)
     
-for x in root[0]:
-    print(x.text)
+    info = well.find('./info')
+    print('  Info:')
+    for data in info.findall('*'):
+        print('    ', data.tag, data.text)
+        
+    samples = well.find('./sample')
+    print('  Samples:')
+    for data in samples.findall('*'):
+        print('    ', data.tag, data.text)
+        
+    print()
+
+    
+    
+    
+
+

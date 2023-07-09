2123933b4578cd86720b0c66f049e461c4ec0972
JTxCo <103444631+JTxCo@users.noreply.github.com>
2023-07-09 17:55:41 -0500

    functions work again

---
+++ azureLocalEM/Code/testFunctions/function_app.py
@@ -0,0 +1,30 @@
+import azure.functions as func
+import logging
+import xml.etree.ElementTree as ET
+
+app = func.FunctionApp()
+@app.function_name(name="HttpTrigger1")
+@app.route(route="hello", auth_level=func.AuthLevel.ANONYMOUS)
+def test_function(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200
+        )
+
+def parse_xml(xml_string):
+    root = ET.fromstring(xml_string)
+    return root

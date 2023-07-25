017ca2be423e2f3e82db180eeec7e68d40839563
JTxCo <103444631+JTxCo@users.noreply.github.com>
2023-07-25 13:34:35 -0600

    testing file upload

--- azureLocalEM/Code/testFunctions/triigerFunctionTest.py
+++ azureLocalEM/Code/testFunctions/triigerFunctionTest.py
@@ -1,32 +1,20 @@
 import requests
 import json
 import os
-#http sending stuff might use later
-# file_path = '../../Data/BioMems-XML-example.xml'
-# with open(file_path, 'rb') as f:
-#     data = f.read()
-#     headers = {'Content-Type': 'application/xml'}
-#     response = requests.post(url, data=data, headers=headers)
 
-# print(response.text)
-# path = '../../Data/'
-# cwd = os.getcwd()
-# filesCWD = os.listdir(cwd)
-# filesPath = os.listdir(path)
-# print(f"Files in {path}: {filesPath}")
-# print(f"Files in swd: {filesCWD}")
+# data = {
+#   "patientInfo": {
+#     "patientID": "abcdefghijklm",
+#     "patientName": "Leslie Nielsen",
+#     "patientDOB": "1926.02.11"
+#   }
+# }
 
+file_path = 'azureLocalEm/Data/jsonEXAMPLE.json'
+with open(file_path, 'rb') as f:
+    json_data = f.read()
 
-data = {
-  "patientInfo": {
-    "patientID": "abcdefghijklm",
-    "patientName": "Leslie Nielsen",
-    "patientDOB": "1926.02.11"
-  }
-}
-
-
-json_data  = json.dumps(data)
+json_data  = json.dumps(json_data)
 headers = {'Content-Type': 'application/json'}
 url = 'http://localhost:7071/api/hello'
 response = requests.post(url, data=json_data, headers=headers) 
